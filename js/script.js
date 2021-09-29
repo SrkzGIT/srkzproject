d44fa3bf942eefbdf58fd74fdf030005ca245a90
Nickolas <example@gmail.com>
2021-09-29 16:17:11 +0300

    lesson13 cycles

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 "use strict";
 
-const nuberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// let num = 50;
 
-const personalMovieDB = {
-    count: nuberOfFilms,
-    movies:{},
-    actors: {},
-    genres: {},
-    privat: false
-};
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while(num < 55);
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+// for (let i = 1; i < 10; i++) {
+//     if ( i===6 ){
+//         // 
+//         continue;
+//     }
+//     console.log(i); 
+// }
---
+++ js/script1.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько его оцените?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+    personalMovieDB.movies[a] = b;
+}
+
+console.log(personalMovieDB);
