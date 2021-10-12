cb9a3330662cd52568ce204b3fbd726d5a0e6133
Nickolas <example@gmail.com>
2021-10-12 17:57:24 +0300

    lesson22spread

--- js/script3.js
+++ js/script3.js
@@ -1,50 +1,50 @@
 "use strict"
 
-function first() {
-    // do something
-    setTimeout(function(){
-        console.log(1);
-    }, 500)
-}
+// function first() {
+//     // do something
+//     setTimeout(function(){
+//         console.log(1);
+//     }, 500)
+// }
 
-function second () {
-    console.log(2);
-}
+// function second () {
+//     console.log(2);
+// }
 
-first();
-second();
+// first();
+// second();
 
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
+// function learnJS(lang, callback) {
+//     console.log(`Я учу: ${lang}`);
+//     callback();
+// }
 
-function done() {
-    console.log('i did this task') 
-}
+// function done() {
+//     console.log('i did this task') 
+// }
 
-learnJS('JavaScript', done);
+// learnJS('JavaScript', done);
 
-const obj = new Object();
+// const obj = new Object();
 
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        background: 'red'
-    },
-    makeTest: function() {
-        console.log("test");
-    }
-};
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         background: 'red'
+//     },
+//     makeTest: function() {
+//         console.log("test");
+//     }
+// };
 
 
-options.makeTest(); figjpdfghp
+// options.makeTest(); 
 
-const {border, background} = options.colors;
-console.log(border);
+// const {border, background} = options.colors;
+// console.log(border);
 
 // console.log(Object.keys(options).length);
 // console.log(options.name);
@@ -67,3 +67,30 @@ console.log(border);
 // }
 // console.log(counter)
 
+
+// arr[99] = 0;
+
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.forEach(function(item, i, arr){
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// arr.pop();
+// arr.push(9);
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+const str = prompt("", "");
+const products = str.split(",");
+products.sort();
+console.log(products.join('; '));
